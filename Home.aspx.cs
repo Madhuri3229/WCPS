be137025d5e3d9370f20d512cbe6c0c9eb0f7e2a
Madhuri3229 <madhuri3229@gmail.com>
2025-06-09 20:19:13 +0530

    Update Home.aspx.cs

--- Home.aspx.cs
+++ Home.aspx.cs
@@ -1,1 +1,22 @@
-
+using System;
+using System.Collections.Generic; using System.Linq;
+using System.Web; using System.Web.UI;
+using System.Web.UI.WebControls; using System.Data;
+using System.Data.SqlClient;
+public partial class _Default : System.Web.UI.Page
+{
+SqlConnection con; SqlCommand cmd;
+protected void Page_Load(object sender, EventArgs e)
+{
+con);
+con = new SqlConnection(db.connection);
+cmd = new SqlCommand("SELECT count(APSEEN) FROM CLAIM WHERE APSEEN='FALSE'",
+con.Open();
+int i = (int)cmd.ExecuteScalar(); con.Close();
+if (i > 0)
+{
+LinkButton2.Visible = true;
+LinkButton2.Text = "You Have " + i.ToString() + " More Pending Claim";
+}
+}
+}
